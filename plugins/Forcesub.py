c9454bb603e550fd7d6d616ed7554a3688fd0904
Tharunfitfun <tharunfitfun204@gmail.com>
2025-01-29 19:41:15 +0530

    Update Forcesub.py

--- plugins/Forcesub.py
+++ plugins/Forcesub.py
@@ -12,24 +12,24 @@ async def forcesub(c, m):
         try:
             user = await c.get_chat_member(UPDATE_CHANNEL, m.from_user.id)
             if user.status == "kicked":
-               await m.reply_text("**Yᴏᴜ ᴀʀᴇ ʙᴀɴɴᴇᴅ ɪɴ Oᴜʀ ᴄʜᴀɴɴᴇʟ Cᴏɴᴛᴀᴄᴛ Aᴅᴍɪɴ 😜**", quote=True)
+               await m.reply_text("**Oooops! Looks like you've been banned from our channel. Contact an admin to sort it out. 😜**", quote=True)
                return
         except UserNotParticipant:
-            buttons = [[InlineKeyboardButton(text='Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ 🔖', url=f"https://t.me/{UPDATE_CHANNEL}")]]
+            buttons = [[InlineKeyboardButton(text='Join Updates Channel 🔖', url=f"https://t.me/{UPDATE_CHANNEL}")]]
             if m.text:
                 if (len(m.text.split(' ')) > 1) & ('start' in m.text):
                     chat_id, msg_id = m.text.split(' ')[1].split('_')
-                    buttons.append([InlineKeyboardButton('🔄 Rᴇғʀᴇsʜ', callback_data=f'refresh+{chat_id}+{msg_id}')])
+                    buttons.append([InlineKeyboardButton('🔄 Refresh', callback_data=f'refresh+{chat_id}+{msg_id}')])
             await m.reply_text(
-                f"Hey {m.from_user.mention(style='md')} ʏᴏᴜ ɴᴇᴇᴅ ᴊᴏɪɴ Mʏ ᴜᴘᴅᴀᴛᴇs ᴄʜᴀɴɴᴇʟ ɪɴ ᴏʀᴅᴇʀ ᴛᴏ ᴜsᴇ ᴍᴇ 😉\n\n"
-                "__Pʀᴇss ᴛʜᴇ Fᴏʟʟᴏᴡɪɴɢ Bᴜᴛᴛᴏɴ ᴛᴏ ᴊᴏɪɴ Nᴏᴡ 👇__",
+                f"Hey {m.from_user.mention(style='md')}! Looks like you need to join my updates channel to continue using me 😉\n\n"
+                "__Press the button below to join now 👇__",
                 reply_markup=InlineKeyboardMarkup(buttons),
                 quote=True
             )
             return
         except Exception as e:
             print(e)
-            await m.reply_text(f"Sᴏᴍᴇᴛʜɪɴɢ Wʀᴏɴɢ. Pʟᴇᴀsᴇ ᴛʀʏ ᴀɢᴀɪɴ ʟᴀᴛᴇʀ ᴏʀ ᴄᴏɴᴛᴀᴄᴛ {owner.mention(style='md')}", quote=True)
+            await m.reply_text(f"Something went wrong! Please try again later or contact {owner.mention(style='md')} 😅", quote=True)
             return
     await m.continue_propagation()
 
@@ -42,40 +42,38 @@ async def refresh_cb(c, m):
             user = await c.get_chat_member(UPDATE_CHANNEL, m.from_user.id)
             if user.status == "kicked":
                try:
-                   await m.message.edit("**Yᴏᴜ ᴀʀᴇ ʙᴀɴɴᴇᴅ ɪɴ Oᴜʀ ᴄʜᴀɴɴᴇʟ Cᴏɴᴛᴀᴄᴛ Aᴅᴍɪɴ 😜**")
+                   await m.message.edit("**Uh-oh! You're banned from our updates channel. Contact the admin to resolve it. 😜**")
                except:
                    pass
                return
         except UserNotParticipant:
-            await m.answer('Yᴏᴜ ᴀʀᴇ ɴᴏᴛ ʏᴇᴛ ᴊᴏɪɴᴇᴅ ᴏᴜʀ ᴄʜᴀɴɴᴇʟ. \nFɪʀsᴛ ᴊᴏɪɴ ᴀɴᴅ ᴛʜᴇɴ ᴘʀᴇss ʀᴇғʀᴇsʜ ʙᴜᴛᴛᴏɴ 🤤', show_alert=True)
+            await m.answer('You need to join our updates channel first. 😏 Join and then click the refresh button. 🤤', show_alert=True)
             return
         except Exception as e:
             print(e)
-            await m.message.edit(f"Sᴏᴍᴇᴛʜɪɴɢ Wʀᴏɴɢ. Pʟᴇᴀsᴇ ᴛʀʏ ᴀɢᴀɪɴ ʟᴀᴛᴇʀ ᴏʀ ᴄᴏɴᴛᴀᴄᴛ{owner.mention(style='md')}")
+            await m.message.edit(f"Something went wrong! Please try again later or contact {owner.mention(style='md')} 😅")
             return
 
     cmd, chat_id, msg_id = m.data.split("+")
     msg = await c.get_messages(int(chat_id), int(msg_id)) if not DB_CHANNEL_ID else await c.get_messages(int(DB_CHANNEL_ID), int(msg_id))
     if msg.empty:
-        return await m.reply_text(f"🥴 Sᴏʀʀʏ ʙʀᴏ ʏᴏᴜʀ ғɪʟᴇ ᴡᴀs ᴍɪssɪɴɢ\n\nPʟᴇᴀsᴇ ᴄᴏɴᴛᴀᴄᴛ ᴍʏ ᴏᴡɴᴇʀ 👉 {owner.mention(style='md')}")
+        return await m.reply_text(f"🥴 Oops! Looks like your file is missing. Please contact my owner 👉 {owner.mention(style='md')}")
 
     caption = msg.caption.markdown
     as_uploadername = (await get_data(str(chat_id))).up_name
     if as_uploadername:
-        if chat_id.startswith('-100'): #if file from channel
+        if chat_id.startswith('-100'):  # if file from channel
             channel = await c.get_chat(int(chat_id))
-            caption += "\n\n\n**--Uᴘʟᴏᴀᴅᴇʀ Dᴇᴛᴀɪʟs:--**\n\n"
-            caption += f"**📢 Cʜᴀɴɴᴇʟ Nᴀᴍᴇ:** __{channel.title}__\n\n"
-            caption += f"**🗣 Usᴇʀ Nᴀᴍᴇ:** @{channel.username}\n\n" if channel.username else ""
-            caption += f"**👤 Cʜᴀɴɴᴇʟ Iᴅ:** __{channel.id}__\n\n"
+            caption += "\n\n\n**--Uploader Details:--**\n\n"
+            caption += f"**📢 Channel Name:** __{channel.title}__\n\n"
+            caption += f"**🗣 Username:** @{channel.username}\n\n" if channel.username else ""
+            caption += f"**👤 Channel ID:** __{channel.id}__\n\n"
         
-        else: #if file not from channel
+        else:  # if file not from channel
             user = await c.get_users(int(chat_id))
-            caption += "\n\n\n**--Uᴘʟᴏᴀᴅᴇʀ Dᴇᴛᴀɪʟs:--**\n\n"
-            caption += f"**🍁 Nᴀᴍᴇ:** [{user.from_user.first_name}](tg://user?id={user.from_user.id})\n\n"
-            caption += f"**🖋 Usᴇʀ Nᴀᴍᴇ:** @{user.username}\n\n" if user.username else ""
-
-
+            caption += "\n\n\n**--Uploader Details:--**\n\n"
+            caption += f"**🍁 Name:** [{user.from_user.first_name}](tg://user?id={user.from_user.id})\n\n"
+            caption += f"**🖋 Username:** @{user.username}\n\n" if user.username else ""
 
     await msg.copy(m.from_user.id, caption=caption)
     await m.message.delete()
